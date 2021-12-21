0a40d85f73248b001439ecfbf30f256447ec7e15
badhaloninja <badhaloninja@badhalo.ninja>
2021-12-21 17:56:42 -0600

    Weee

--- HeadlessTweaks/ImpulsePass.cs
+++ HeadlessTweaks/ImpulsePass.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HarmonyLib;
+﻿using HarmonyLib;
 using FrooxEngine.LogiX.ProgramFlow;
-using FrooxEngine.LogiX;
 using FrooxEngine;
-using BaseX;
 namespace HeadlessTweaks
 {
     class ImpulsePass
     {
+        public static string TagTriggerPrefix = "$Headless.";
+
+
         public static void Init(Harmony harmony)
         {
             HeadlessTweaks.Msg("Initializing ImpulsePass");
@@ -20,7 +16,7 @@ namespace HeadlessTweaks
 
             harmony.Patch(dynamicImpulseTrigger, prefix: new HarmonyMethod(dynamicImpulseTriggerPrefix));
         }
-        public static string TagTriggerPrefix = "$Headless.";
+
         [HarmonyPatch(typeof(DynamicImpulseTrigger), "Run")]
         class TypedImpulsePass
         {
@@ -28,8 +24,8 @@ namespace HeadlessTweaks
             {
                 if (!__instance.Tag.Evaluate().StartsWith(TagTriggerPrefix)) return true;
                 string TargetMethodName = __instance.Tag.Evaluate().Substring(TagTriggerPrefix.Length);
-
-                var foundMethod = typeof(DynamicImpulseMethods).GetMethod(TargetMethodName);
+                
+                var foundMethod = typeof(ImpulsePassMethods).GetMethod(TargetMethodName);
                 if (foundMethod == null)
                 {
                     HeadlessTweaks.Msg("DynamicImpulseMethod '" + TargetMethodName + "' not found");
@@ -38,41 +34,9 @@ namespace HeadlessTweaks
                 HeadlessTweaks.Msg("Found DynamicImpulseMethod '" + TargetMethodName + "'");
                 Slot parameterSlot = __instance.TargetHierarchy.EvaluateRaw();
                 
-                foundMethod.Invoke(null, new object[] { __instance, parameterSlot });
+                foundMethod.Invoke(null, new object[] { __instance.World, parameterSlot });
                 return false;
             }
-        }
-
-        public class DynamicImpulseMethods // *Public* methods are available to logix *:*)
-        {
-            public static void NameSlot(DynamicImpulseTrigger __instance, Slot parameterSlot)
-            {
-                Slot slot = FetchParameter<Slot>(parameterSlot, "Target");
-                string name = FetchParameter<string>(parameterSlot, "Name");
-             
-                if (slot != null) slot.Name = name;
-                return;
-            }
-        }
-        [GenericTypes(GenericTypes.Group.NeosPrimitives, new System.Type[] { typeof(Slot), typeof(User) })]
-        public static bool FetchParameter<T>(Slot slot, string variableName, out T value)
-        {
-            value = Coder<T>.Default;
-            DynamicVariableSpace space = slot.FindSpace("HeadlessExec");
-            if (space != null)
-            {
-                return space.TryReadValue<T>(variableName, out value);
-            }
-            return false;
-        }
-        public static T FetchParameter<T>(Slot slot, string variableName)
-        {
-            T value;
-            if(FetchParameter<T>(slot, variableName, out value))
-            {
-                return value;
-            }
-            return Coder<T>.Default;
         }
     }
 }
---
+++ HeadlessTweaks/ImpulsePassMethods.cs
@@ -0,0 +1,47 @@
+﻿using FrooxEngine;
+using BaseX;
+namespace HeadlessTweaks
+{
+    public class ImpulsePassMethods
+    {
+        public static void NameSlot(World world, Slot parameterSlot)
+        {
+            Slot slot = Utilities.FetchParameter<Slot>(parameterSlot, "Target");
+            string name = Utilities.FetchParameter<string>(parameterSlot, "Name");
+
+            if (slot != null) slot.Name = name;
+            return;
+        }
+        public static void SaveWorld(World world, Slot parameterSlot)
+        {
+            Userspace.SaveWorldAuto(world, SaveType.Overwrite, false);
+            return;
+        }
+    }
+
+
+    public class Utilities
+    {
+        [GenericTypes(GenericTypes.Group.NeosPrimitives, new System.Type[] { typeof(Slot), typeof(User) })]
+        public static bool FetchParameter<T>(Slot slot, string variableName, out T value)
+        {
+            value = Coder<T>.Default;
+            DynamicVariableSpace space = slot.FindSpace("HeadlessExec");
+            if (space != null)
+            {
+                return space.TryReadValue<T>(variableName, out value);
+            }
+            return false;
+        }
+        public static T FetchParameter<T>(Slot slot, string variableName)
+        {
+            T value;
+            if (FetchParameter<T>(slot, variableName, out value))
+            {
+                return value;
+            }
+            return Coder<T>.Default;
+        }
+    }
+
+}
