24aa65f18916fe6cc7fa1a269312e742cdb8aa52
badhaloninja <badhaloninja@badhalo.ninja>
2026-01-18 10:26:38 -0600

    fix(SmartAutosave): rewrite smart autosave patch to a transpiler

--- HeadlessTweaks/SmartAutosave.cs
+++ HeadlessTweaks/SmartAutosave.cs
@@ -1,8 +1,10 @@
-﻿using HarmonyLib;
-using System;
+﻿using FrooxEngine;
+using FrooxEngine.Headless;
+using HarmonyLib;
 using System.Collections.Generic;
-using FrooxEngine;
-using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 
 namespace HeadlessTweaks
 {
@@ -10,58 +12,72 @@ namespace HeadlessTweaks
     {
         internal static void Init(Harmony harmony)
         {
-            var target = typeof(Userspace).GetMethod(nameof(Userspace.CanSave));
-            //var target = typeof(Userspace).GetMethod("WorldSaveAuto");
-            var prefix = typeof(SmartAutosave).GetMethod(nameof(CanSaveOverride));
+            // Thank you zonni from stack overflow for finally teaching me AsyncStateMachineAttribute exists
 
-            harmony.Patch(target, prefix: new HarmonyMethod(method: prefix));
+            // Get the existing HandlerLoop method from WorldHandler
+            var handlerLoopMethod = typeof(WorldHandler).GetMethod("HandlerLoop", BindingFlags.Instance | BindingFlags.NonPublic);
+            // Because it is async we want to get it's state machine, we can find this by it's AsyncStateMachineAttribute
+            var stateMachineAttr = handlerLoopMethod.GetCustomAttribute<AsyncStateMachineAttribute>();
+            // And from that state machine we can get the generated type and it's MoveNext method
+            var moveNextMethod = stateMachineAttr.StateMachineType.GetMethod("MoveNext", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Get our transpiler function
+            var transpiler = typeof(SmartAutosave).GetMethod(nameof(Transpile));
+
+            // Patch world handler's handler loop
+            harmony.Patch(moveNextMethod, transpiler: new(transpiler));
         }
 
-        static Type handlerLoop;
+        /// <summary>
+        /// This allows saving once more after players have left the session
+        /// </summary>
+        static readonly Dictionary<World, bool> ShouldSaveOnceMore = [];
 
-        // A map of worlds to booleans 
-        static readonly Dictionary<World, bool> dontSave = [];
-        
-        
-        public static bool CanSaveOverride(World world, ref bool __result)
+        static readonly MethodInfo CanSaveMethod = typeof(Userspace).GetMethod(nameof(Userspace.CanSave));
+        static readonly MethodInfo ShouldSaveMethod = typeof(SmartAutosave).GetMethod(nameof(ShouldAutoSave));
+
+
+        public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
         {
-            if (!HeadlessTweaks.SmartAutosaveEnabled.GetValue())
-                return true;
-
-            dontSave.TryAdd(world, false);
-
-            // Awful
-            // Get call stack
-            StackTrace stackTrace = new();
-            // Get calling method name
-            var frame = stackTrace.GetFrame(2);
-            var decType = frame.GetMethod().DeclaringType;
-            
-            if (handlerLoop == null && decType.Name.Contains("HandlerLoop"))
-                handlerLoop = decType;
-
-            if (handlerLoop != decType || world.UserCount > 1)
+            // Loop over every instruction and return them in order
+            foreach (var instruction in instructions)
             {
-                //ResoniteMod.Msg("Try to save");
-
-                if(world.UserCount > 1 && dontSave[world])
+                // Find the call for Userspace.CanSave inside of handler loop and replace it with our ShouldAutoSave method
+                // This should only be in the if check for auto saving
+                if(instruction.Calls(CanSaveMethod))
                 {
-                    dontSave[world] = false;
+                    yield return new(OpCodes.Call, ShouldSaveMethod);
+                    continue;
                 }
-                
-                return true;
-            }
-            
 
-            if (!dontSave[world])
+                // Return the original instruction
+                yield return instruction;
+            }
+        }
+
+        public static bool ShouldAutoSave(World world)
+        {
+            // If we can't save just return false here and don't continue
+            if (!Userspace.CanSave(world)) return false;
+
+            // If our save patch is disabled continue to save the world and skip the rest
+            if (!HeadlessTweaks.SmartAutosaveEnabled.GetValue()) return true;
+
+            // Ensure there is an entry for this world in the list
+            ShouldSaveOnceMore.TryAdd(world, false);
+
+            // If there is more than just the host in the world then autosave should run
+            if(world.UserCount > 1 || ShouldSaveOnceMore[world])
             {
-                //ResoniteMod.Msg("Last save");
-                dontSave[world] = true;
+                // If there are currently players in the session (asside from host)
+                // Then we should save again next time even if there are no more players
+                ShouldSaveOnceMore[world] = world.UserCount > 1;
+                
+                // Continue to save the world
                 return true;
             }
 
-            //ResoniteMod.Msg("Dont try to save");
-            __result = false;
+            // Don't save the world
             return false;
         }
     }
